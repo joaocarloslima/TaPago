e3ebd40fa4e67db06c53fe46b5ec5a9892e4ef0d
joaocarloslima <joaocarloslima@me.com>
2024-02-29 12:03:32 -0300

    criação do projeto

---
+++ src/main/java/br/com/fiap/tapago/controller/CategoriaController.java
@@ -0,0 +1,30 @@
+package br.com.fiap.tapago.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CategoriaController {
+    
+    @RequestMapping(
+        method = RequestMethod.GET, 
+        path = "/categoria", 
+        produces = "application/json"
+    )
+    @ResponseBody
+    public String index(){
+        return 
+        """
+            [
+                {
+                    "id": 1,
+                    "nome": "Alimentação",
+                    "icone": "fast-food"
+                }
+            ]
+        """;
+    }
+    
+}
