38bdf3e0bfa89a49e55cffc2397787c768716e0b
joaocarloslima <joaocarloslima@me.com>
2024-05-16 11:41:14 -0300

    deploy config

--- src/main/java/br/com/fiap/tapago/config/DatabaseSeeder.java
+++ src/main/java/br/com/fiap/tapago/config/DatabaseSeeder.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import br.com.fiap.tapago.model.Categoria;
 import br.com.fiap.tapago.model.Movimentacao;
@@ -14,6 +15,7 @@ import br.com.fiap.tapago.repository.CategoriaRepository;
 import br.com.fiap.tapago.repository.MovimentacaoRepository;
 
 @Configuration
+@Profile("dev")
 public class DatabaseSeeder implements CommandLineRunner{
 
     @Autowired
